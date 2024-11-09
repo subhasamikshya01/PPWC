817730add56c0b81b18819ad32627e80b9b139fb
subhasamikshya01 <165682929+subhasamikshya01@users.noreply.github.com>
2024-11-09 09:49:22 +0530

    Add files via upload

---
+++ msq1a.c
@@ -0,0 +1,8 @@
+ #include<stdio.h>
+ int main(){
+ 	float s,a;
+ 	printf("enter the length of 1 side of the square:- ");
+ 	scanf("%f",&s);
+ 	a=s*s;
+ 	printf("The area of a square whose side length is %2f cm is %2f square cm\n",s,a);
+ }
---
+++ msq1b.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#define pi 3.14159
+int main(){
+	float r,a,c;
+	int num_circ;
+	char circ_name;
+}
